540d1c92e3aeb1ce0b7d5723981430c4abc0729d
amirayat <amirayat20@gmail.com>
2023-04-28 22:33:31 +0330

    add sanic

---
+++ sanic/app.py
@@ -0,0 +1,32 @@
+import os
+import psycopg2
+import psycopg2.extras
+from dotenv import load_dotenv
+from sanic import Sanic, json
+
+
+load_dotenv()
+
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = int(os.getenv('DB_PORT'))
+
+
+app = Sanic(name=__name__) 
+
+conn = psycopg2.connect(f"dbname='{DB_NAME}' user='{DB_USER}' host='{DB_HOST}' password='{DB_PASSWORD}'")
+
+
+@app.route("/countries/")
+def list_countries(request):
+    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute("SELECT * FROM countries")
+    countries = cur.fetchall()
+    return json(countries)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 8000, access_log=False, workers=5)
